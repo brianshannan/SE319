2c3767ac88687f69fbd7a0898514ebb7089e6c06
bshannan <brianshannan@gmail.com>
2014-09-06 20:20:25 -0500

    Added validation around the seconds box

--- src/exercise1/HelloWorldSwingFrame3.java
+++ src/exercise1/HelloWorldSwingFrame3.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JCheckBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.Timer;
@@ -21,12 +23,31 @@ public class HelloWorldSwingFrame3 extends HelloWorldSwingFrame {
 	
 	@Override
 	protected JPanel formFontSizePanel() {
+		class TimeFieldListener implements ActionListener {
+			private void showErrorMessage() {
+				JOptionPane.showMessageDialog(new JFrame(), "Not a valid value.");
+			}
+			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				try {
+					int num = Integer.parseInt(timeInputField.getText());
+					if(1 <= num && num <= 10) {
+						showErrorMessage();
+					}
+				} catch(NumberFormatException e) {
+					showErrorMessage();
+				}
+			}
+		}
+		
 		JPanel panel = super.formFontSizePanel();
 		JLabel clearAfterLabel = new JLabel(" clear after: ");
 		
 		// These have to be initialized earlier as this function is called in super()
 		timeCheckBox = new JCheckBox();
 		timeInputField = new JTextField("thing");
+		timeInputField.addActionListener(new TimeFieldListener());
 		
 		panel.add(timeCheckBox);
 		panel.add(clearAfterLabel);
