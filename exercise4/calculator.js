afb1be3c4c15370f6fa1c96f196a1526587e279f
Brian Shannan <brian.shannan@workiva.com>
2014-10-05 12:49:48 -0500

    layout doneish

--- exercise4/calculator.js
+++ exercise4/calculator.js
@@ -1,16 +1,18 @@
 var Calculator = function() {
     var self = this;
+    self.opperator = null;
     self.memoryValue = 0;
 
     self.root = $('<table/>', {
         id: 'calcTable',
         border: 2
     });
-    self.root.append('<tr><td><input id="calc-input" type="text" value="0"></td></tr>');
+    self.root.append('<tr><td colspan="4"><input id="calc-input" type="text" value="0"></td></tr>');
 
     self.blankFunc = function() {};
 
     self.getDisplay = function() {
+        self.init();
         return self.root;
     };
 
@@ -31,22 +33,25 @@ var Calculator = function() {
             type: 'button',
             value: value
         });
-        if(action != null) {
-            self.button.click(action);
-        }
+        self.button.click(action);
 
         self.getHTML = function() {
-            return self.button;
-        }
-
-        self.setOnClick = function(func) {
-            self.button.click(func);
+            return $('<td></td>').append(self.button);
+            // return '<td>'+ self.button + '</td>';
         }
     }
+
+    self.calculate = function (){
+
+    }
+
+
 
     self.addButton = new Button('plus', '+', function() {
         var sel = $('#calc-input');
         var first = parseFloat(sel.val());
+
+        // ops
         return function() {
             var second = 'thing';
             return first + second;
@@ -55,7 +60,7 @@ var Calculator = function() {
 
     self.memPlus = new Button('mem-plus', 'M+', function() {
         var sel = $('#calc-input');
-        self.memoryValue += sel.val();
+        self.memoryValue += parseFloat(sel.val());
     });
 
     self.memRead = new Button('mem-read', 'MR', function() {
@@ -69,7 +74,7 @@ var Calculator = function() {
 
     self.clear = new Button('clear', 'C', function() {
         var sel = $('#calc-input');
-        sel.val("");
+        sel.val("0");
     })
 
     self.numberButtonFunction = function(num) {
@@ -78,8 +83,52 @@ var Calculator = function() {
         };
     };
 
-    for(var i = 0; i < 10; i++) {
-        var numButton = new Button(i, i, self.numberButtonFunction(i));
-        self.root.append(numButton.getHTML());
+    self.opperatorButtonFunction = function (opperator) {
+        return function() {
+            self.calculate(opperator)
+        }
+    };
+
+    // for(var i = 0; i < 10; i++) {
+    //     var numButton = new Button(i, i, self.numberButtonFunction(i));
+    //     self.root.append(numButton.getHTML());
+    // }
+
+    self.init = function () {
+        var buttons = [9,8,7,"+",6,5,4,"-",3,2,1,'*',0,'.','=','/','C','MR','MC','M+'];
+        var row1 = $('<tr></tr>');
+        row1.append(new Button('7', '7', self.numberButtonFunction(7)).getHTML());
+        row1.append(new Button('8', '8', self.numberButtonFunction(8)).getHTML());
+        row1.append(new Button('9', '9', self.numberButtonFunction(9)).getHTML());
+        // Append plus
+        self.root.append(row1);
+
+        var row2 = $('<tr></tr>');
+        row2.append(new Button('4', '4', self.numberButtonFunction(4)).getHTML());
+        row2.append(new Button('5', '5', self.numberButtonFunction(5)).getHTML());
+        row2.append(new Button('6', '6', self.numberButtonFunction(6)).getHTML());
+        // Append minus
+        self.root.append(row2);
+
+        var row3 = $('<tr></tr>');
+        row3.append(new Button('1', '1', self.numberButtonFunction(1)).getHTML());
+        row3.append(new Button('2', '2', self.numberButtonFunction(2)).getHTML());
+        row3.append(new Button('3', '3', self.numberButtonFunction(3)).getHTML());
+        // Append mul
+        self.root.append(row3);
+
+        var row4 = $('<tr></tr>');
+        row4.append(new Button('0', '0', self.numberButtonFunction(0)).getHTML());
+        row4.append(new Button('.', '.', self.numberButtonFunction('.')).getHTML());
+        // Append equals
+        // Append div
+        self.root.append(row4);
+
+        var row5 = $('<tr></tr>');
+        row5.append(self.clear.getHTML());
+        row5.append(self.memRead.getHTML());
+        row5.append(self.memClear.getHTML());
+        row5.append(self.memPlus.getHTML());
+        self.root.append(row5);
     }
 };
