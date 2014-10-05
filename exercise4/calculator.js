4917fb0a32f8b5dd31a455b483b05fc51a828ccf
Brian Shannan <brian.shannan@workiva.com>
2014-10-05 12:57:28 -0500

    layout done

--- exercise4/calculator.js
+++ exercise4/calculator.js
@@ -7,9 +7,6 @@ var Calculator = function() {
         id: 'calcTable',
         border: 2
     });
-    self.root.append('<tr><td colspan="4"><input id="calc-input" type="text" value="0"></td></tr>');
-
-    self.blankFunc = function() {};
 
     self.getDisplay = function() {
         self.init();
@@ -37,7 +34,6 @@ var Calculator = function() {
 
         self.getHTML = function() {
             return $('<td></td>').append(self.button);
-            // return '<td>'+ self.button + '</td>';
         }
     }
 
@@ -48,14 +44,31 @@ var Calculator = function() {
 
 
     self.addButton = new Button('plus', '+', function() {
-        var sel = $('#calc-input');
-        var first = parseFloat(sel.val());
+        // var sel = $('#calc-input');
+        // var first = parseFloat(sel.val());
+        //
+        // // ops
+        // return function() {
+        //     var second = 'thing';
+        //     return first + second;
+        // }
+        return self.calculate;
+    });
 
-        // ops
-        return function() {
-            var second = 'thing';
-            return first + second;
-        }
+    self.subButton = new Button('minus', '-', function() {
+        return self.calculate;
+    });
+
+    self.mulButton = new Button('mul', '*', function() {
+        return self.calculate;
+    });
+
+    self.divButton = new Button('div', '/', function() {
+        return self.calculate;
+    });
+
+    self.equalButton = new Button('equals', '=', function() {
+        return self.calculate;
     })
 
     self.memPlus = new Button('mem-plus', 'M+', function() {
@@ -89,39 +102,35 @@ var Calculator = function() {
         }
     };
 
-    // for(var i = 0; i < 10; i++) {
-    //     var numButton = new Button(i, i, self.numberButtonFunction(i));
-    //     self.root.append(numButton.getHTML());
-    // }
-
     self.init = function () {
-        var buttons = [9,8,7,"+",6,5,4,"-",3,2,1,'*',0,'.','=','/','C','MR','MC','M+'];
+        self.root.children().remove();
+        self.root.append('<tr><td colspan="4"><input id="calc-input" type="text" value="0"></td></tr>');
         var row1 = $('<tr></tr>');
         row1.append(new Button('7', '7', self.numberButtonFunction(7)).getHTML());
         row1.append(new Button('8', '8', self.numberButtonFunction(8)).getHTML());
         row1.append(new Button('9', '9', self.numberButtonFunction(9)).getHTML());
-        // Append plus
+        row1.append(self.addButton.getHTML());
         self.root.append(row1);
 
         var row2 = $('<tr></tr>');
         row2.append(new Button('4', '4', self.numberButtonFunction(4)).getHTML());
         row2.append(new Button('5', '5', self.numberButtonFunction(5)).getHTML());
         row2.append(new Button('6', '6', self.numberButtonFunction(6)).getHTML());
-        // Append minus
+        row2.append(self.subButton.getHTML());
         self.root.append(row2);
 
         var row3 = $('<tr></tr>');
         row3.append(new Button('1', '1', self.numberButtonFunction(1)).getHTML());
         row3.append(new Button('2', '2', self.numberButtonFunction(2)).getHTML());
         row3.append(new Button('3', '3', self.numberButtonFunction(3)).getHTML());
-        // Append mul
+        row3.append(self.mulButton.getHTML());
         self.root.append(row3);
 
         var row4 = $('<tr></tr>');
         row4.append(new Button('0', '0', self.numberButtonFunction(0)).getHTML());
         row4.append(new Button('.', '.', self.numberButtonFunction('.')).getHTML());
-        // Append equals
-        // Append div
+        row4.append(self.equalButton.getHTML());
+        row4.append(self.divButton.getHTML());
         self.root.append(row4);
 
         var row5 = $('<tr></tr>');
