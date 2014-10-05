56e617ccbd1d40f770efb8820243a377009a5ab1
Brian Shannan <brian.shannan@workiva.com>
2014-10-05 11:19:56 -0500

    next try

--- exercise4/calculator.js
+++ exercise4/calculator.js
@@ -1,7 +1,18 @@
 var Calculator = function() {
     var self = this;
     self.memoryValue = 0;
+
+    self.root = $('<table/>', {
+        id: 'calcTable',
+        border: 2
+    });
+    self.root.append('<tr><td><input id="calc-input" type="text" value=""></td></tr>');
+
     self.blankFunc = function() {};
+
+    self.getDisplay = function() {
+        return self.root;
+    }
 
     var Button = function(id, value, action) {
         var self = this;
@@ -14,46 +25,26 @@ var Calculator = function() {
             self.button.click(action);
         }
 
+        self.getHTML = function() {
+            return self.button;
+        }
+
         self.setOnClick = function(func) {
             self.button.click(func);
         }
     }
 
-    self.equalButton = new Button('equals', '=', self.blankFunc);
+    self.numberButtonFunction = function(num) {
+        return function() {
+            var sel = $('#calc-input');
+            var val = sel.val();
+            val = val + num;
+            sel.val(val);
+        };
+    }
 
-    self.addButton = new Button('add', '+', function() {
-        var first = 'thing';  // TODO get value
-        // TODO clear text field
-        self.equalButton.setOnClick(function() {
-            var second = 'thing';
-            // Set text field to first + second
-        });
-    });
-
-    self.subButton = new Button('sub', '-', function() {
-        var first = 'thing';  // TODO get value
-        // TODO clear text field
-        self.equalButton.setOnClick(function() {
-            var second = 'thing';
-            // Set text field to first - second
-        });
-    });
-
-    self.mulButton = new Button('mul', '*', function() {
-        var first = 'thing';  // TODO get value
-        // TODO clear text field
-        self.equalButton.setOnClick(function() {
-            var second = 'thing';
-            // Set text field to first * second
-        });
-    });
-
-    self.divButton = new Button('div', '/', function() {
-        var first = 'thing';  // TODO get value
-        // TODO clear text field
-        self.equalButton.setOnClick(function() {
-            var second = 'thing';
-            // Set text field to first / second
-        });
-    });
+    for(var i = 0; i < 10; i++) {
+        var numButton = new Button(i, i, self.numberButtonFunction(i));
+        self.root.append(numButton.getHTML());
+    }
 };
