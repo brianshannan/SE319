50c3456e2cf33dfb5d358a19d2c90b2703c0694b
Eric Larssen <elarssen@iastate.edu>
2014-10-12 23:08:30 -0500

    Added Library Additions

--- Exercise05/booksLibrary.js
+++ Exercise05/booksLibrary.js
@@ -1,38 +1,100 @@
+'use strict';
 
-var Book = function () {
-
+var Book = function (name, author, isbn) {
+    this.name = name;
+    this.author = author;
+    this.isbn = isbn;
 };
 
+var Shelf = function (size) {
+    this.size = size;
+    this.books = [];
+};
 
 var Library = function () {
     this.shelves = [];
-
 };
 
-Libary.prototype.addNCopies = function addNBook(book, n) {
+Library.prototype.addNCopies = function(book, n) {
     for (var i = 0; i < this.shelves.length ; i++) {
-        for (var j = 0; j < Shelf.prototype.size ; j++)
-            if (n == 0){
+        for (var j = 0; j < Shelf.prototype.size ; j++) {
+            if (n === 0){
                 return;
             }
-            if (this.shelves[i].books[j] === null) {
+            if (this.shelves[i].books[j] == null) {
                 this.shelves[i].books[j] = book;
                 n--;
             }
-        });
-    });
+        }
+    }
     if (n > 0) {
         this.shelves.push(new Shelf());
-        addNBook(book, n)
+        this.addNCopies(book, n);
     }
 };
 
-var Shelf = function () {
-    this.size = size;
-    this.books = [this.size];
-};
+Library.prototype.findLocation = function(name) {
+    for (var i = 0; i < this.shelves.length ; i++) {
+        for (var j = 0; j < this.shelves[i].books.length ; j++) {
+            var book = this.shelves[i].books[j];
+            if (book.name === name){
+                return {shelf: i, row: j};
+            }
+        }
+    }
+}
 
-Shelf.protoype.size = 10;
-Shelf.protoype.setSize = function (size) {
+Library.prototype.getBookByName = function(name) { 
+    for (var i = 0; i < this.shelves.length ; i++) {
+        for (var j = 0; j < this.shelves[i].books.length ; j++) {
+            var book = this.shelves[i].books[j];
+            if (book.name === name){
+                return book;
+            }
+        }
+    }
+}
+
+Library.prototype.removeBook = function(name) {
+    var count = 0;
+    for (var i = 0; i < this.shelves.length ; i++) {
+        for (var j = 0; j < this.shelves[i].books.length ; j++) {
+            var book = this.shelves[i].books[j];
+            if (book.name === name){
+                this.shelves[i].books[j] = null;
+                count++;
+            }
+        }
+    }
+
+    for (var i = 0; i < this.shelves.length ; i++) {
+        var nulls = 0;
+        for (var j = 0; j < this.shelves[i].books.length ; j++) {
+            var book = this.shelves[i].books[j];
+            if (book == null){
+                nulls++;
+            }
+        }
+        if (nulls === this.shelves[i].books.length){
+            this.shelves.splice(i);
+        }
+    }
+
+    return count;
+}
+
+Library.prototype.getShelves = function() {
+    return this.shelves;
+}
+
+//Accepts List of Objects i.e. [{name: bookName, count: bookCount}]
+Library.prototype.addMultiple = function(books) {
+    $.forEach(books, function(book) {
+        this.addNCopies(book.name, book.count);
+    });
+}
+
+Shelf.prototype.size = 10;
+Shelf.prototype.setSize = function (size) {
     Shelf.prototype.size = size;
 };
