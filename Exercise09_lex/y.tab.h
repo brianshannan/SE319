bc19320a14d38c209d5a6beb0edcf786ae0e7ef0
Brian Shannan <brian.shannan@workiva.com>
2014-11-05 14:44:20 -0600

    broke some stuff but on a better track

--- Exercise09_lex/y.tab.h
+++ Exercise09_lex/y.tab.h
@@ -1,8 +1,11 @@
 #define ELEM_START 0
-#define ELEM_EMPTY 1
-#define ELEM_CLOSE 2
+#define ELEM_NAME_NON_EMPTY 1
+#define ELEM_NAME_EMPTY 2
 #define ATTR 3
-#define TEXT 4
-#define XML_DEC_OPEN 5
-#define XML_DEC_CLOSE 6
-#define XML_DEC_ATTR 7
+#define ELEM_END 4
+#define TEXT 5
+#define ELEM_CLOSE 6
+#define ELEM_CLOSE_NAME 7
+#define XML_DEC_OPEN 8
+#define XML_DEC_CLOSE 9
+#define XML_DEC_ATTR 10
